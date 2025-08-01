b00bb6da0ab7b9d5ec059f759fa597ec4759e9e7
Jakub Boukal <www.bagr@gmail.com>
2025-08-01 16:26:33 +0200

    feat: extend admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -3,16 +3,37 @@ from django.contrib import admin
 from . import models
 
 
+class MovieMovieCreatorInline(admin.TabularInline):
+    model = models.MovieCreator
+    extra = 0
+    fields = ("creator", "role")
+    autocomplete_fields = ("creator",)
+    ordering = ("role", "creator__name")
+    readonly_fields = ("creator",)
+
+
+class CreatorMovieCreatorInline(admin.TabularInline):
+    model = models.MovieCreator
+    extra = 0
+    fields = ("movie", "role")
+    autocomplete_fields = ("movie",)
+    ordering = ("role", "movie__title")
+    readonly_fields = ("movie",)
+
+
 @admin.register(models.Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "release_year", "rating")
-    search_fields = ("title", "actors__name")
-    list_filter = ("release_year", "rating")
-    ordering = ("-release_year", "title")
+    search_fields = ("normalized_title",)
+    inlines = (MovieMovieCreatorInline,)
+    list_filter = ("release_year",)
+    ordering = ("-rating",)
 
 
 @admin.register(models.Creator)
 class CreatorAdmin(admin.ModelAdmin):
     list_display = ("name",)
-    search_fields = ("name",)
+    search_fields = ("normalized_name",)
+    inlines = (CreatorMovieCreatorInline,)
+    list_filter = ("movie_creators__role",)
     ordering = ("name",)
